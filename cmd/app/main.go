e9e0aa6b5a499f908ca704648251e70c29f688ee
Camden Withrow <withrowcamden@gmail.com>
2024-09-18 16:04:27 -0500

    add subdomain admin

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,15 +1,65 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"os"
 
+	_ "github.com/joho/godotenv/autoload"
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+type (
+	Host struct {
+		Echo *echo.Echo
+	}
 )
 
 func main() {
-	e := echo.New()
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
+	hosts := map[string]*Host{}
+	port, exists := os.LookupEnv("PORT")
+	if !exists {
+		port = "4321"
+	}
+
+	hostDomain, exists := os.LookupEnv("HOST")
+	if !exists {
+		hostDomain = "localhost"
+	}
+
+	admin := echo.New()
+	admin.Use(middleware.Logger())
+	admin.Use(middleware.Recover())
+	hosts[fmt.Sprintf("admin.%s:%s", hostDomain, port)] = &Host{admin}
+
+	admin.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Admin")
 	})
-	e.Logger.Fatal(e.Start(":1323"))
+
+	site := echo.New()
+	site.Use(middleware.Logger())
+	site.Use(middleware.Recover())
+	hosts[fmt.Sprintf("%s:%s", hostDomain, port)] = &Host{site}
+
+	site.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Website")
+	})
+
+	e := echo.New()
+	e.Any("/*", func(c echo.Context) (err error) {
+		req := c.Request()
+		res := c.Response()
+		host := hosts[req.Host]
+
+		if host == nil {
+			err = echo.ErrNotFound
+		} else {
+			host.Echo.ServeHTTP(res, req)
+		}
+
+		return
+	})
+
+	e.Logger.Fatal(e.Start(":4321"))
 }
