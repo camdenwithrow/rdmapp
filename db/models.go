3f780d68ca734b90fbf1ee07115fba9584270dfd
Camden Withrow <withrowcamden@gmail.com>
2024-09-19 13:42:58 -0500

    add models for data

--- db/models.go
+++ db/models.go
@@ -1,14 +1,70 @@
 package db
 
+import "time"
+
 type Feature struct {
-	ID          int    `json:"id"`
-	Name        string `json:"name"`
-	Description string `json:"description"`
-	Status      string `json:"status"`
-	Priority    int    `json:"priority"`
-	TargetDate  string `json:"target_date"`
-	UpVotes     int    `json:"upvotes"`
-	DateAdded   string `json:"date_added"`
-	CreatedAt   string `json:"created_at"`
-	UpdatedAt   string `json:"updated_at"`
+	ID          int       `json:"id"`
+	Name        string    `json:"name"`
+	Description string    `json:"description"`
+	Status      string    `json:"status"`
+	Priority    int       `json:"priority"`
+	UpVotes     int       `json:"upvotes"`
+	DateAdded   time.Time `json:"date_added"`
+	CreatedAt   time.Time `json:"created_at"`
+	UpdatedAt   time.Time `json:"updated_at"`
+}
+
+type User struct {
+	ID        uint      `json:"id"`
+	Email     string    `json:"email"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
+
+type Owner struct {
+	ID        uint      `json:"id"`
+	UserId    uint      `json:"user_id"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
+
+type Project struct {
+	ID        uint      `json:"id"`
+	OwnerID   uint      `json:"owner_id"`
+	Name      string    `json:"name"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
+
+type Roadmap struct {
+	ID        uint      `json:"id"`
+	ProjectID uint      `json:"project_id"`
+	Slug      string    `json:"slug"`
+	Title     string    `json:"title"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
+
+type RoadmapContributor struct {
+	ID        uint      `json:"id"`
+	RoadmapID uint      `json:"roadmap_id"`
+	UserID    uint      `json:"user_id"`
+	Role      string    `json:"role"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
+
+type Vote struct {
+	ID        uint      `json:"id"`
+	FeatureID uint      `json:"feature_id"`
+	UserID    uint      `json:"user_id"`
+	CreatedAt time.Time `json:"created_at"`
+}
+
+type Comment struct {
+	ID          uint      `json:"id"`
+	FeatureID   uint      `json:"feature_id"`
+	UserID      uint      `json:"user_id"`
+	CommentText string    `json:"comment_text"`
+	CreatedAt   time.Time `json:"created_at"`
 }
