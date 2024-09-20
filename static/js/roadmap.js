efa3c037574ee4863e4ddb3d1c87ffd93e60d6dc
Camden Withrow <withrowcamden@gmail.com>
2024-09-19 23:55:02 -0500

    add dialog

--- static/js/roadmap.js
+++ static/js/roadmap.js
@@ -1,5 +1,17 @@
 "use strict";
 
+function darkenBackground(background) {
+	background.classList.remove("bg-zinc-50/0")
+	background.classList.add("bg-zinc-900/80")
+	document.documentElement.classList.add("bg-zinc-900/80")
+}
+
+function removeDarkenedBackground(background) {
+	background.classList.add("bg-zinc-50/0")
+	background.classList.remove("bg-zinc-900/80")
+	document.documentElement.classList.remove("bg-zinc-900/80")
+}
+
 function getSheetEls(id) {
 	const container = document.getElementById(`${id}-sheet-container`)
 	const background = document.getElementById(`${id}-sheet-bg`)
@@ -7,16 +19,21 @@ function getSheetEls(id) {
 	return [container, background, sheet]
 }
 
+function getDialogEls(id) {
+	console.log(`${id}-dialog-container`)
+	const container = document.getElementById(`${id}-dialog-container`)
+	const background = document.getElementById(`${id}-dialog-bg`)
+	const dialog = document.getElementById(`${id}-dialog`)
+	return [container, background, dialog]
+}
+
 function openSheet(id) {
 	const [container, background, sheet] = getSheetEls(id)
-	console.log(`${id}-sheet-container`)
 
 	container.classList.remove("hidden")
 
 	setTimeout(() => {
-		background.classList.remove("bg-zinc-50/0")
-		background.classList.add("bg-zinc-900/80")
-		document.documentElement.classList.add("bg-zinc-900/80")
+		darkenBackground(background)
 
 		sheet.classList.remove("translate-x-full")
 		sheet.classList.add("translate-x-0")
@@ -26,14 +43,38 @@ function openSheet(id) {
 function closeSheet(id) {
 	const [container, background, sheet] = getSheetEls(id)
 
-	background.classList.add("bg-zinc-50/0")
-	background.classList.remove("bg-zinc-900/80")
-	document.documentElement.classList.remove("bg-zinc-900/80")
+	removeDarkenedBackground(background)
 
 	sheet.classList.add("translate-x-full")
 	sheet.classList.remove("translate-x-0")
 
 	setTimeout(() => {
 		container.classList.add("hidden")
-	}, 500)
+	}, 400)
+}
+
+function openDialog(id) {
+	const [container, background, dialog] = getDialogEls(id)
+
+	container.classList.remove("hidden")
+
+	setTimeout(() => {
+		darkenBackground(background)
+
+		dialog.classList.remove("scale-90", "opacity-0")
+		dialog.classList.add("scale-100", "opacity-100")
+	}, 1)
+}
+
+function closeDialog(id) {
+	const [container, background, dialog] = getDialogEls(id)
+
+	removeDarkenedBackground(background)
+
+	dialog.classList.add("scale-90", "opacity-0")
+	dialog.classList.remove("scale-100", "opacity-100")
+
+	setTimeout(() => {
+		container.classList.add("hidden")
+	}, 400)
 }
